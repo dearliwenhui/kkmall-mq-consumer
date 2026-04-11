9dbb4f6a6289a131ac8b9919c45d4e9802b53bd7
Dave.Li <dave.li@aswatson.com>
2026-04-11 20:14:25 +0800

    增加订单超时

--- src/test/java/com/ab/kkmallmqconsumer/scheduler/ConfigRefreshSchedulerTests.java
+++ src/test/java/com/ab/kkmallmqconsumer/scheduler/ConfigRefreshSchedulerTests.java
@@ -1,5 +1,6 @@
 package com.ab.kkmallmqconsumer.scheduler;
 
+import com.ab.kkmallmqconsumer.consumer.OrderTimeoutConsumerManager;
 import com.ab.kkmallmqconsumer.consumer.RocketMqConsumerManager;
 import com.ab.kkmallmqconsumer.service.TopicConfigService;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,14 @@ class ConfigRefreshSchedulerTests {
     void shouldReconcileConsumersEvenWhenConfigSignatureIsUnchanged() {
         TopicConfigService topicConfigService = mock(TopicConfigService.class);
         RocketMqConsumerManager consumerManager = mock(RocketMqConsumerManager.class);
+        OrderTimeoutConsumerManager orderTimeoutConsumerManager = mock(OrderTimeoutConsumerManager.class);
         when(topicConfigService.refresh()).thenReturn(false);
 
-        ConfigRefreshScheduler scheduler = new ConfigRefreshScheduler(topicConfigService, consumerManager);
+        ConfigRefreshScheduler scheduler = new ConfigRefreshScheduler(topicConfigService, consumerManager, orderTimeoutConsumerManager);
         scheduler.refresh();
 
         verify(topicConfigService).refresh();
         verify(consumerManager).refreshConsumers();
+        verify(orderTimeoutConsumerManager).refreshConsumer();
     }
 }
